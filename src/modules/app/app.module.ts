20a5010be1cadb0be694a0cb0b5a2f686c49e2bb
Mubashir N <mubashir.nazer@nibavlifts.com>
2024-11-12 18:54:46 +0530

    Databae connected

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,14 +4,37 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from '../hello/hello.module';
 import { WorldModule } from '../world/world.module';
 import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
+import { User } from '../../entities/user.entity';
+import { Post } from '../../entities/post.entity';
 
 @Module({
-  imports: [HelloModule, WorldModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [User, Post],
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([User, Post]),
+    HelloModule,
+    WorldModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(private dataSource: DataSource) {}
+
+  // This will be called after the application initializes
+  async onModuleInit() {
+    const isConnected = await this.checkDatabaseConnection();
+    if (isConnected) {
+      console.log('Database is connected!');
+    } else {
+      console.log('Database connection failed!');
+    }
+  }
+
+  private async checkDatabaseConnection(): Promise<boolean> {
+    try {
+      // Try running a simple query to check if the DB is reachable
+      await this.dataSource.query('SELECT 1');
+      return true;
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      return false;
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
